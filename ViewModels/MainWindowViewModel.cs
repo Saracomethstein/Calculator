a3a570b894d5b2d49aa75f2449694ea645561008
Saracomethstein <adel.sevastianov@yandex.ru>
2023-09-14 20:05:18 +0300

    Create text input

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -5,9 +5,31 @@ namespace Calculator.ViewModels
 {
     internal class MainWindowViewModel : ViewModelBase
     {
+        private string _func;
         private RelayCommand _getCloseApplication;
         private RelayCommand _getMinimizedCommand;
+        private RelayCommand _enterDigitOneCommand;
+        private RelayCommand _enterDigitTwoCommand;
+        private RelayCommand _enterDigitThreeCommand;
+        private RelayCommand _enterDigitFourCommand;
+        private RelayCommand _enterDigitFiveCommand;
+        private RelayCommand _enterDigitSixCommand;
+        private RelayCommand _enterDigitSevenCommand;
+        private RelayCommand _enterDigitEightCommand;
+        private RelayCommand _enterDigitNineCommand;
+        private RelayCommand _enterDigitZeroCommand;
 
+        public string Func
+        {
+            get { return _func; }
+            set
+            { 
+                _func = value;
+                NotifyPropertyChanged(nameof(Func));
+            }
+        }
+
+        #region Command
         public RelayCommand GetCloseApplication
             => _getCloseApplication ?? (_getCloseApplication = new RelayCommand(() =>
             {
@@ -25,5 +47,18 @@ namespace Calculator.ViewModels
                 }
             }));
 
+        public RelayCommand EnterDigitOneCommand
+            => _enterDigitOneCommand ?? (_enterDigitOneCommand = new RelayCommand(() =>
+            {
+                Func += "1";
+            }));
+
+        public RelayCommand EnterDigitTwoCommand
+            => _enterDigitTwoCommand ?? (_enterDigitTwoCommand = new RelayCommand(() =>
+            {
+                Func += "2";
+            }));
+
+        #endregion
     }
 }
